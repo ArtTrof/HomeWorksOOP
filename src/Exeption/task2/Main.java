756d6d4086156388bd87c479e2cfede4a326c53d
Artem Trofymenko <randallsng@gmail.com>
2022-12-06 20:37:22 +0100

    new commit

--- src/Exeption/task2/Main.java
+++ src/Exeption/task2/Main.java
@@ -1,13 +1,57 @@
 package Exeption.task2;
 
 import java.sql.SQLOutput;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.Year;
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.Vector;
+import java.util.*;
 
 public class Main {
+    public static boolean isThisDateValid(String dateToValidate, String dateFormat) {
+        if (dateToValidate == null) {
+            return false;
+        }
+
+        SimpleDateFormat sdf = new SimpleDateFormat(dateFormat);
+        sdf.setLenient(false);
+
+        try {
+            Date date = sdf.parse(dateToValidate);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
+        Worker[] worker = new Worker[4];
+        Scanner in = new Scanner(System.in);
+        int[] seniority = new int[4];
+        for (int i = 0; i < worker.length; i++) {
+            String initial, pos, year;
+            System.out.println("Input initials ");
+            initial = in.nextLine();
+            System.out.println("Input position ");
+            pos = in.nextLine();
+            System.out.println("Input year ");
+            year = in.nextLine();
+            isThisDateValid(year, "yyyy");
+            worker[i] = new Worker(initial, pos, year);
+
+
+            int yearNow = Calendar.getInstance().get(Calendar.YEAR);
+
+            seniority[i] = yearNow - Integer.parseInt(year);
+        }
+
+        for (int i = 0; i < worker.length; i++) {
+            System.out.println(worker[i].toString());
+            System.out.println("Staj :" + seniority[i]);
+
+        }
+
+
 
     }
 }
--- src/Exeption/task2/Worker.java
+++ src/Exeption/task2/Worker.java
@@ -1,7 +1,21 @@
 package Exeption.task2;
 
+import org.w3c.dom.ls.LSOutput;
+
 import java.time.Year;
 
 public class Worker {
 
+    String fio,position,year;
+
+    public Worker(String fio, String store, String year) {
+        this.fio = fio;
+        this.position = store;
+        this.year = year;
+    }
+
+    @Override
+    public String toString(){
+        return "initials :"+this.fio+" store :"+this.position + "year:" + this.year;
+    }
 }
