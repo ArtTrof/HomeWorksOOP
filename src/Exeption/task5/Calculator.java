5be3ea347149b1151b042adcb4e434be51427f2b
Artem Trofymenko <randallsng@gmail.com>
2022-12-08 11:33:27 +0100

    new commit

---
+++ src/Exeption/task5/Calculator.java
@@ -0,0 +1,27 @@
+package Exeption.task5;
+
+public class Calculator {
+
+    public Double add (double a,double b){
+     return a+b;
+    }
+
+    public Double sub (double a,double b){
+
+        return a-b;
+    }
+
+    public Double mul (double a,double b){
+        return a*b;
+    }
+
+    public Double div (double a,double b) {
+        double c;
+        if (b != 0) {
+            c = a / b;
+        } else {
+            throw new ArithmeticException("No div on 0");
+        }
+        return c;
+    }
+}
---
+++ src/Exeption/task5/Main.java
@@ -0,0 +1,43 @@
+package Exeption.task5;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in  = new Scanner(System.in);
+        Calculator calc =  new Calculator();
+        System.out.println("input a: ");
+        double a = in.nextDouble();
+        System.out.println("input b: ");
+        double b = in.nextDouble();
+
+        System.out.println("Input operator( +,-,*,/");
+        String operator = in.nextLine();
+
+        switch(operator) {
+            case ("+"):
+                System.out.println("Answer" + calc.add(a, b));
+                break;
+            case ("-"):
+                System.out.println("Answer" + calc.sub(a, b));
+                break;
+            case ("*"):
+                System.out.println("Answer" + calc.mul(a, b));
+                break;
+            case ("/"):
+                try {
+                    System.out.println("Answer" + calc.div(a, b));
+                } catch (ArithmeticException e) {
+                    System.out.println(e.getMessage());
+                }
+                break;
+            default:
+                System.out.println("Wrong operator");
+        }
+
+
+
+    }
+}
